e209793aaab25a85409838b567f91d7d688fc3cf
DevFaizaan <DevFaizaan.b@gmail.com>
2022-08-31 10:20:20 +0100

    routes and controller completed

--- api/routes/movieRoutes.js
+++ api/routes/movieRoutes.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getMovie,
+  setMovie,
+  updateMovie,
+  deleteMovie,
+} = require('../controllers/receiptController');
+
+router.route('/').get(getMovie).post(setMovie);
+router.route('/:id').delete(deleteMovie).put(updateMovie);
 
 module.exports = router;

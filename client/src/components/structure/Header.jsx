cff3040de81af86b7848f77242326af604d509a3
Kiran Evans <thekiranevans@gmail.com>
2022-09-01 17:20:11 +0100

    Update Header.jsx

--- client/src/components/structure/Header.jsx
+++ client/src/components/structure/Header.jsx
@@ -5,8 +5,8 @@ const Header = (props) => {
 
     return (
         <div className="header">
+            <div className="logo"><Link to='/'><img alt="Logo" src="https://cdn.discordapp.com/attachments/768054278933184533/1014837355909226497/qa-cinemas-logo-transp.png" width={70} height={45}/></Link></div>
             <div className="nav">
-                <div className="logo"><Link to='/'><img alt="Logo" src="https://cdn.discordapp.com/attachments/768054278933184533/1014837355909226497/qa-cinemas-logo-transp.png" width={70} height={45}/></Link></div>
                 <div className="navLinks">
                     <Link to='/'>Home</Link>
                     <Link to='/listings'>Now Showing</Link>

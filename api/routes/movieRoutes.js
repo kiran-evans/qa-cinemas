22f0d16bf541ee909b475c28f4ae4cd0a6ad3bc6
kiran-evans <thekiranevans@gmail.com>
2022-09-01 12:07:02 +0100

    KE: Search now works.

--- api/routes/movieRoutes.js
+++ api/routes/movieRoutes.js
@@ -5,11 +5,9 @@ const {
   setMovie,
   updateMovie,
   deleteMovie,
-  searchMovies,
 } = require('../controller/movieController');
 
 router.route('/').get(getMovie).post(setMovie);
 router.route('/:id').delete(deleteMovie).put(updateMovie);
-router.route('/search').get(searchMovies);
 
 module.exports = router;

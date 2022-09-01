eac7735a46dfc480f44a4ed32f8dc094b2bf7521
Frederick Clay <fredfmclay@gmail.com>
2022-09-01 15:34:53 +0100

    set up Discussion backend using express

---
+++ api/controller/discussionController.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+
+const { Discussions } = require('../model/discussionModel');
+
+router.get('/getAllRelated', (req, res, next) => {
+    Discussions.find({"movie": req.body.movie})
+        .then((result) => res.status(200).json(result))
+        .catch((err) => next(err));
+});
+
+router.post('/postNewDiscussion', (req, res, next) => {
+    if (!req.body || Object.keys(req.body).length < 5) return next({ status: 400, msg: 'Incomplete/No Body Provided' });
+
+    Discussions.create(req.body)
+        .then((result) => res.status(201).json(result))
+        .catch((err) => next(err));
+});
+
+router.delete('deleteByMovie', (req, res, next) => {
+    const {movie} = req.params;
+
+    Discussions.deleteMany({"movie": movie})
+        .then((result) => res.status(204).json(result))
+        .catch((err) => next(err))
+});
+
+router.delete('deleteById', (req, res, next) => {
+    const { id } = req.params;
+
+    Discussions.findByIdAndDelete(id)
+        .then((result) => res.status(204).json(result))
+        .catch((err) => next(err))
+});
+
+module.exports = router;

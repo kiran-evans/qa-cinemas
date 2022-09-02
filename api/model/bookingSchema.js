1f09c3741b409ff0bae977425dcfdb6ec572da60
DevFaizaan <DevFaizaan.b@gmail.com>
2022-09-02 16:29:47 +0100

    movie booking and movie posters

---
+++ api/model/bookingSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const formSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    seats: {
+      type: String,
+      required: true,
+    },
+    ticketType: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model('contactForm', formSchema);
--- api/routes/movieRoutes.js
+++ api/routes/movieRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {
   getMovie,
+  getMovieByID,
   setMovie,
   updateMovie,
   deleteMovie,
 } = require('../controller/movieController');
 
 router.route('/').get(getMovie).post(setMovie);
-router.route('/:id').delete(deleteMovie).put(updateMovie);
+router.route('/:id').delete(deleteMovie).put(updateMovie).get(getMovieByID);
 
 module.exports = router;
